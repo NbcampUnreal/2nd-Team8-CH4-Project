c1b004ffac54ac6d4ee1daaa66d3f41e378584ac
안장훈 <wkdgns135@gmail.com>
2025-04-02 10:38:24 +0900

    [Add] EOS login

---
+++ Source/LastStand/EOS/LSLoginSubsystem.cpp
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LSLoginSubsystem.h"
+#include  "OnlineSubsystemUtils.h"
+#include "OnlineSubsystem.h"
+#include "Interfaces/OnlineUserInterface.h"
+
+void ULSLoginSubsystem::LoginWithEOS(const FString& UserId, const FString& Token, const FString& LoginType)
+{
+    IOnlineSubsystem* SubsystemRef = Online::GetSubsystem(GetWorld());
+
+    if(SubsystemRef)
+    {
+        IOnlineIdentityPtr Identity = SubsystemRef->GetIdentityInterface();
+        if (Identity.IsValid())
+        {
+            FOnlineAccountCredentials Credentials;
+            Credentials.Id = UserId;
+            Credentials.Token = Token;
+            Credentials.Type = LoginType;
+            Identity->OnLoginCompleteDelegates->AddUObject(this, &ULSLoginSubsystem::LoginWithEOSComplete);
+            Identity->Login(0, Credentials);
+        }
+    }
+}
+
+void ULSLoginSubsystem::GetPlayerUse(const FString& UserId, const FString& Token, const FString& LoginType)
+{
+    
+}
+
+void ULSLoginSubsystem::LoginWithEOSComplete(int32 LocalUserNum, bool bWasSuccessful, const FUniqueNetId& UserId,
+                                             const FString& ErrorString)
+{
+    if (bWasSuccessful)
+    {
+        UE_LOG(LogTemp, Log, TEXT("Login successful for user: %s"), *UserId.ToString());
+    }else
+    {
+        UE_LOG(LogTemp, Log, TEXT("Login failed: %s"), *ErrorString);
+    }
+}
---
+++ Source/LastStand/EOS/LSLoginSubsystem.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "LSLoginSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LASTSTAND_API ULSLoginSubsystem : public UGameInstanceSubsystem
+{
+    GENERATED_BODY()
+
+    UFUNCTION(BlueprintCallable, Category = "LSEOSLoginSubsystem")
+    void LoginWithEOS(const FString& UserId, const FString& Token, const FString& LoginType);
+
+    UFUNCTION(BlueprintCallable, Category = "LSEOSLoginSubsystem")
+    void GetPlayerUse(const FString& UserId, const FString& Token, const FString& LoginType);
+
+    void LoginWithEOSComplete(int32 LocalUserNum, bool bWasSuccessful, const FUniqueNetId& UserId, const FString& ErrorString);
+};
