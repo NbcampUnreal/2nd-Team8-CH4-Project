c1d698f5232e1499c58f1a68b8789e18a09457e9
balamwind <yyo0033@naver.com>
2025-04-05 01:07:50 +0900

    [Add] 캐릭터 픽 구현 완료
    캐릭터 픽을 구현 완료했습니다.
    선택한 캐릭터 이름을 넘기는건 맵 선택에서 같이 추가할 예정입니다.

--- Source/LastStand/Data/LSGameDataSubSystem.cpp
+++ Source/LastStand/Data/LSGameDataSubSystem.cpp
@@ -1,25 +1,27 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "LSGameDataSubSystem.h"
+#include "LSGameDataSubsystem.h"
+#include "LSCharacterDataAsset.h"
 
 
-ULSGameDataSubSystem::ULSGameDataSubSystem()
+ULSGameDataSubsystem::ULSGameDataSubsystem()
 {
     LoadData();
 }
 
-void ULSGameDataSubSystem::Initialize(FSubsystemCollectionBase& Collection)
+void ULSGameDataSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
     Super::Initialize(Collection);
-    
-    checkf(CharacterTable, TEXT("Failed to Load CharacterTable"));
+
+    checkf(Character, TEXT("Failed to Load CharacterTable"));
 }
 
-void ULSGameDataSubSystem::LoadData()
+void ULSGameDataSubsystem::LoadData()
 {
-    static ConstructorHelpers::FObjectFinder<UDataTable> CharacterTableLoader(
-        TEXT("/Script/Engine.DataTable'/Game/LastStand/Data/BT_Character.BT_Character'"));
-    if (CharacterTableLoader.Succeeded())
-        CharacterTable = CharacterTableLoader.Object;
+    static ConstructorHelpers::FObjectFinder<ULSCharacterDataAsset> CharacterDataLoader(
+        TEXT("/Script/LastStand.LSCharacterDataAsset'/Game/LastStand/Data/DA_Character.DA_Character'"));
+
+    if (CharacterDataLoader.Object)
+        Character = CharacterDataLoader.Object;
 }
