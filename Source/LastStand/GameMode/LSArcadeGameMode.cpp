87f50f0b126f9ffd41b3a26fe67544dbd1d8e9fb
arbasang <154686862+arbasang@users.noreply.github.com>
2025-04-04 10:49:39 +0900

    [Init] 아케이드 게임모드 생성

---
+++ Source/LastStand/GameMode/LSArcadeGameMode.cpp
@@ -0,0 +1,2 @@
+#include "GameMode/LSArcadeGameMode.h"
+
---
+++ Source/LastStand/GameMode/LSArcadeGameMode.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameMode/LSBattleGameMode.h"
+#include "LSArcadeGameMode.generated.h"
+
+UCLASS()
+class LASTSTAND_API ALSArcadeGameMode : public ALSBattleGameMode
+{
+	GENERATED_BODY()
+	
+};
