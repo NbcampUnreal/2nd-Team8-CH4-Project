110aa8c79f1c078ac262b25eaeab85f047e68298
wkdgns135 <wkdgns135@naver.com>
2025-04-16 21:45:34 +0900

    [Add] 리더보드 구현

--- Source/LastStand/EOS/LSEOSDataSubsystem.h
+++ Source/LastStand/EOS/LSEOSDataSubsystem.h
@@ -7,20 +7,55 @@
 #include "Interfaces/OnlineLeaderboardInterface.h"
 #include "LSEOSDataSubsystem.generated.h"
 
-/**
- * 
- */
+USTRUCT(BlueprintType)
+struct FLeaderboardEntry
+{
+    GENERATED_USTRUCT_BODY()
+    
+    UPROPERTY(BlueprintReadWrite)
+    FString NickName;
+
+    UPROPERTY(BlueprintReadWrite)
+    int32 Rank;
+
+    UPROPERTY(BlueprintReadWrite)
+    FString Value;
+
+    FLeaderboardEntry()
+    {
+        
+    }
+    
+    FLeaderboardEntry(const FString& InNickName, int32 InRank, const FString& InValue)
+        : NickName(InNickName), Rank(InRank), Value(InValue)
+    {
+    }
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEOSLeaderboardUpdated, const TArray<FLeaderboardEntry>&, LeaderboardEntries);
+
 UCLASS()
 class LASTSTAND_API ULSEOSDataSubsystem : public UGameInstanceSubsystem
 {
     GENERATED_BODY()
 
 public:
+    UFUNCTION(BlueprintCallable, Category = "LSEOSDataSubsystem")
     void UpdateStat(FString StatName, int32 StatValue);
+    UFUNCTION(BlueprintCallable, Category = "LSEOSDataSubsystem")
     void QueryLeaderboardGlobal(FName LeaderboardName = "WINLEADERBOARD");
     void QueryLeaderboardFriends(FString StatName, FName LeaderboardName = "WINLEADERBOARD" );
-
+    
 private:
-    FDelegateHandle QueryLeaderboardDelegateHandle; 
     void HandleQueryLeaderboarComplete(bool bWasSuccessful, FOnlineLeaderboardReadRef LeaderboardReadRef);	
+    TArray<FLeaderboardEntry> ParseLeaderboardToArray(const FOnlineLeaderboardReadRef& LeaderboardReadRef);
+
+public:
+    UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "LSEOSDataSubsystem")
+    FOnEOSLeaderboardUpdated OnEOSLeaderboardUpdated;
+    
+private:
+    FDelegateHandle QueryLeaderboardDelegateHandle;
+    FString CurrentStatName;
+    
 };
